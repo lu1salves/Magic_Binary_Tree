b359d732dd5c3f0e0d38c29a4c2b6c8220ba3c9a
Luís Henrique <luis_alves@discente.ufg.br>
2023-11-10 11:25:37 -0300

    Funções e arquivos
    Adicionando as funções de árvore e possivelmente a estrutura de arquivos do projeto

--- binary_tree.py
+++ binary_tree.py
@@ -1,2 +1,27 @@
 
-class No:
+class Nodulo:
+
+    def __init__(self, value):
+        self.Value = value
+        self.Ponteiro_esquerda = None
+        self.Ponteiro_direita = None
+        self.Dano_ataque = None
+        self.Dano_defesa = None
+        self.Eh_magico = None
+        self.Descricao = ""
+    
+    def Posicao_ponteiro_esquerda()
+        
+    def Posicao_ponteiro_direita()
+        
+    def Inserir_no()
+        
+    #funções de percorrer a árvore
+
+    def Percorrer_Pofixo()
+    
+    def Percorrer_Infixo()
+        
+    def Percorrer_Prefixo()
+        
+    
